05ddaaff88e9930de5b9ed0feac8fd32dcfa5230
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-07-07 21:41:36 +0300

    commit-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { Route, Routes, Link, NavLink, Navigate, useNavigate } from "react-router-dom";
+import { FiUser, FiHeart } from "react-icons/fi";
+import { PiBasket } from "react-icons/pi";
+
 import './App.css';
+import Register from "./components/register";
 
 function App() {
+
+  const [user, setUser] = useState(false);
+  const [login, setLogin] = useState(false);
+  const [errMessage, setErrMessage] = useState({});
+
+  const loginHandle = () => {
+    
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" />
+      <Route path="register" element={<Register />} />
+    </Routes>
+    // <div className="App">
+    //   <div className="nav">
+    //       <h3>Anasayfa</h3>
+    //       <h3>Sana Özel</h3>
+    //       <h3>Beğenebileceklerin</h3>
+    //     <button onClick={loginHandle}  className="btn-1">
+    //       <FiUser />
+    //     </button>
+    //       <PiBasket />
+    //       <FiHeart />
+    //   </div>
+    // </div>
   );
 }
 
